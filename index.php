0d1c044002d91e00a9b2e586b38f2e847488abca
Rechmand M <rachmand777@gmail.com>
2023-04-18 01:24:30 +0700

    change field in index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['us']) && $_SESSION['login'] == true) {
         <meta name="description" content="">
         <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
         <meta name="generator" content="Hugo 0.84.0">
-        <title>Bimbingan Belajar Rechmand M</title>
+        <title>Bimbingan Belajar By Rechmand M</title>
         <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
         <!-- Bootstrap core CSS -->
         <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -59,7 +59,7 @@ if (isset($_SESSION['us']) && $_SESSION['login'] == true) {
             </div>
         </div>
 
-
+        <!-- script -->
         <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
